8e32457f8ec4dd76f90bb06d7bde220576225b4c
JacK9004 <kjushkin@gmail.com>
2024-07-08 22:21:48 +0900

    feat: create componenets related modules

--- apps/jkbooks-api/src/app.module.ts
+++ apps/jkbooks-api/src/app.module.ts
@@ -6,6 +6,7 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver } from '@nestjs/apollo';
 import { AppResolver } from './app.resolver';
 import { ComponentsModule } from './components/components.module';
+import { DatabaseModule } from './database/database.module';
 
 
 
@@ -18,6 +19,7 @@ import { ComponentsModule } from './components/components.module';
       autoSchemaFile: true,
     }),
     ComponentsModule,
+    DatabaseModule,
   ],
   controllers: [AppController],
   providers: [AppService, AppResolver],
