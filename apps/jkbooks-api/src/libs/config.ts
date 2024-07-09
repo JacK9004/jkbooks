640bc82773e394ad8d1eb274721eba71c4b10438
JacK9004 <kjushkin@gmail.com>
2024-07-10 01:46:06 +0900

    feat: develop getAllMembersByAdmin & updateMemberByAdmin

--- apps/jkbooks-api/src/libs/config.ts
+++ apps/jkbooks-api/src/libs/config.ts
@@ -1,6 +1,7 @@
 import { ObjectId } from "bson";
 
 export const avilableAgentSorts = ['createAt', 'updateAt', 'memberLikes', 'memberViews', 'memberRank'];
+export const avilableMembertSorts = ['createAt', 'updateAt', 'memberLikes', 'memberViews'];
 
 export const shapeIntoMongoObjectId = (target: any) => {
     return typeof target === 'string' ? new ObjectId(target) : target;
