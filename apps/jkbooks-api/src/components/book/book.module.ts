0a418740b8b0e34b7bf347c5bbade5c5bdb52634
JacK9004 <kjushkin@gmail.com>
2024-07-10 03:23:13 +0900

    feat: modification of book module

--- apps/jkbooks-api/src/components/book/book.module.ts
+++ apps/jkbooks-api/src/components/book/book.module.ts
@@ -1,4 +1,21 @@
 import { Module } from '@nestjs/common';
+import { BookResolver } from './book.resolver';
+import { BookService } from './book.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import BookSchema from '../../schemas/book.model';
+import { AuthModule } from '../auth/auth.module';
+import { ViewModule } from '../view/view.module';
 
-@Module({})
+@Module({
+  imports: [MongooseModule.forFeature([
+    {
+      name: 'Book', 
+      schema: BookSchema
+    }
+  ]), 
+  AuthModule,
+  ViewModule
+],
+  providers: [BookResolver, BookService]
+})
 export class BookModule {}
