8833dbbb6045cd0b7b16d14715ef307f6ef9aa32
JacK9004 <kjushkin@gmail.com>
2024-07-12 23:17:33 +0900

    fix: modify like interaction related logic

--- apps/jkbooks-api/src/components/follow/follow.service.ts
+++ apps/jkbooks-api/src/components/follow/follow.service.ts
@@ -6,7 +6,7 @@ import { MemberService } from '../member/member.service';
 import { Direction, Message } from '../../libs/enums/common.enum';
 import { FollowInquiry } from '../../libs/dto/follow/follow.input';
 import { T } from '../../libs/types/common';
-import { lookupFollowerData, lookupFollowingData } from '../../libs/config';
+import { lookupAuthMemberLiked, lookupFollowerData, lookupFollowingData } from '../../libs/config';
 import { exec } from 'child_process';
 
 @Injectable()
@@ -77,7 +77,7 @@ export class FollowService {
                         list: [
                             { $skip: (page - 1) * limit },
                             { $limit: limit },
-                            //meLiked
+                            lookupAuthMemberLiked(memberId, "followingId"),
                             //meFollowed
                             lookupFollowingData,
                             { $unwind: '$followingData' },
@@ -110,7 +110,7 @@ export class FollowService {
                         list: [
                             { $skip: (page - 1) * limit },
                             { $limit: limit },
-                            //meLiked
+                            lookupAuthMemberLiked(memberId, "followerId"),
                             //meFollowed
                             lookupFollowerData,
                             { $unwind: '$followerData' },
